3f52fe7132ad481514e17cdf601ecfb9757d7a5b
Rishabh Kaushick <kaushick.r@northeastern.edu>
2023-11-22 11:36:57 -0500

    Added the skeleton of the test file for Factory Design Pattern.

---
+++ Lab8_DesignPatterns/FactoryDesignPatternDemo/test/factorydesignpatterntest/ShapeFactoryTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package factorydesignpatterntest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The ShapeFactoryTest is a JUnit Test class for testing the ShapeFactory class.
+ * @author rishabhkaushick
+ */
+public class ShapeFactoryTest {
+    
+    public ShapeFactoryTest() {
+    }
+    
+    /**
+     * The getShapeTest() method is used to test the getShape() method from ShapeFactory class.
+     * @version 1.0
+     */
+    @Test
+    public void getShapeTest(){
+        // work in progress
+    }
+}
