30ad7770be44ac40592cbb2461535934957b31c3
Rishabh Kaushick <kaushick.r@northeastern.edu>
2023-11-21 21:29:03 -0500

    Added the skeleton Test file for the Singleton Design Pattern.

---
+++ Lab8_DesignPatterns/SingletonDesignPatternDemo/test/singletondesignpatterndemotest/SingleObjectTest.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package singletondesignpatterndemotest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The SingleObjectTest is a JUnit Test class for testing the SingleObject class.
+ * @author rishabhkaushick
+ */
+public class SingleObjectTest {
+    
+    public SingleObjectTest() {
+    }
+    
+    /**
+     * The getInstanceTest() method is used to test the getInstance() method from SingleObject class.
+     * @version 1.0
+     */
+    @Test
+    public void getInstanceTest(){
+        // under construction
+    }
+    
+    /**
+     * The showMessageTest() method is used to test the showMessage() method from SingleObject class.
+     * @version 1.0
+     */
+    @Test
+    public void showMessageTest(){
+        // under construction
+    }
+}
