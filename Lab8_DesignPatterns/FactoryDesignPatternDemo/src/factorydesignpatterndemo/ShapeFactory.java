78e141f1ae130dff424ec513ef07d6346a9fdc01
Rishabh Kaushick <kaushick.r@northeastern.edu>
2023-11-22 11:35:48 -0500

    Added the java source files for the Factory Design Pattern.

---
+++ Lab8_DesignPatterns/FactoryDesignPatternDemo/src/factorydesignpatterndemo/ShapeFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factorydesignpatterndemo;
+
+/**
+ *
+ * @author rishabhkaushick
+ */
+public class ShapeFactory {
+    
+    public Shape getShape(String shapeType){
+        if(shapeType == null){
+            return null;
+        }
+        if(shapeType.equalsIgnoreCase("CIRCLE")){
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQAURE")){
+            return new Square();
+        }
+        
+        return null;
+    }
+}
